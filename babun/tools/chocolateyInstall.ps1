1da59bea164f17c8210c9da8586e48200d633b05
Leandro Conca De Luccia <lluccia@gmail.com>
2018-01-02 14:52:54 -0200

    waits for installation script to finish

--- babun/tools/chocolateyInstall.ps1
+++ babun/tools/chocolateyInstall.ps1
@@ -1,27 +1,36 @@
-function RunBabunInstall($unzipPath)
-{
-    $babunpath = Join-Path $env:USERPROFILE ".babun"
-    $updateBat = Join-Path $babunpath "update.bat"
-    if (Test-Path $updateBat)
-    {
-        start-process $updateBat -Wait
-    }
-    else
-    {
-        $setupBat = Join-Path $unzipPath "babun-1.2.0"
-        $setupBat = Join-Path $setupBat "install.bat"
-        start-process $setupBat #-Wait
-    }
-}
-
+$babunpath = Join-Path $env:USERPROFILE ".babun"
 $unzipPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$packageArgs = @{
-  packageName = 'babun'
-  url = 'http://projects.reficio.org/babun/download/babun-1.2.0-dist.zip';
-  checksumType = 'sha256'
-  checksum = '89C0A51EA1D995BE5B04B24CD795BA3EBA713B293B43991F0C87E29300429B14'
-  unzipLocation = $unzipPath
+
+function InstallBabun {
+    $packageArgs = @{
+        packageName = 'babun'
+        url = 'http://projects.reficio.org/babun/download/babun-1.2.0-dist.zip';
+        checksumType = 'sha256'
+        checksum = '89C0A51EA1D995BE5B04B24CD795BA3EBA713B293B43991F0C87E29300429B14'
+        unzipLocation = $unzipPath
+    }
+
+    Install-ChocolateyZipPackage @packageArgs
+
+    $babunUnzippedPath = Join-Path $unzipPath "babun-1.2.0"
+    $setupBat = Join-Path $babunUnzippedPath "install.bat"
+    $setupBatUnattended = Join-Path $babunUnzippedPath "install-unattended.bat"
+
+    Get-Content $setupBat |
+        foreach-object {$_ -replace '"%BABUN_HOME%"\\babun.bat', 'rem -- should not run babun --' } |
+        foreach-object {$_ -replace 'pause', 'rem -- should not pause --' } |
+        Set-Content $setupBatUnattended
+
+    start-process $setupBatUnattended -Wait
 }
 
-Install-ChocolateyZipPackage @packageArgs
-RunBabunInstall $unzipPath
+function BabunIsInstalled {
+    return Test-Path $babunpath
+}
+
+if (BabunIsInstalled) {
+    Write-Host "Babun is already installed! Skipping..."
+} else {
+    Write-Host "Executing installation script..."
+    InstallBabun
+}
