5a01c71435ccd88f5cccfc635b5863c0addb90e3
Leandro Conca De Luccia <lluccia@gmail.com>
2018-01-06 01:52:45 -0200

    forces home directory to internal cygwin directory when HOME variable is set

--- babun/tools/chocolateyInstall.ps1
+++ babun/tools/chocolateyInstall.ps1
@@ -1,6 +1,31 @@
 $babunpath = Join-Path $env:USERPROFILE ".babun"
 $unzipPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
+function ConfigureBabunToUseInternalHomeDir {
+    Write-Host "Configuring Babun to use internal home directory..."
+
+    $etcPath = Join-Path $babunpath "cygwin/etc"
+
+    $etcProfile = Join-Path $etcPath "profile"
+    $etcProfileReplaced = Join-Path $etcPath "profile_rep"
+
+    (Get-Content $etcProfile -Raw).Replace('USER="$(/usr/bin/id -un)"', "USER=`"`$(/usr/bin/id -un)`"`n  HOME=`$(eval echo `"~`$USER`")") |
+        Set-Content $etcProfileReplaced
+
+     Move-Item -Force $etcProfileReplaced $etcProfile
+
+     $etcZProfile = Join-Path $etcPath "zprofile"
+     $etcZProfileReplaced = Join-Path $etcPath "zprofile_rep"
+
+     (Get-Content $etcZProfile -Raw).Replace('USER="$(/usr/bin/id -un)"', "USER=`"`$(/usr/bin/id -un)`"`n  HOME=`$(eval echo `"~`$USER`")") |
+         Set-Content $etcZProfileReplaced
+
+      Move-Item -Force $etcZProfileReplaced $etcZProfile
+
+      $zshrc = Join-Path $env:USERPROFILE ".zshrc"
+      New-Item $zshrc -type file
+}
+
 function InstallBabun {
     $packageArgs = @{
         packageName = 'babun'
@@ -21,7 +46,19 @@ function InstallBabun {
         foreach-object {$_ -replace 'pause', 'rem -- should not pause --' } |
         Set-Content $setupBatUnattended
 
-    start-process $setupBatUnattended -Wait
+    if ($env:HOME) {
+        # temporary disable HOME variable during installation because it is not supported by babun
+        $homeEnvVar = $env:HOME
+        $env:HOME = ""
+    }
+
+    Write-Host "Executing installation script..."
+    start-process $setupBatUnattended -Wait -WindowStyle Minimized
+
+    if ($homeEnvVar) {
+        $env:HOME = $homeEnvVar
+        ConfigureBabunToUseInternalHomeDir
+    }
 }
 
 function FixFilePermissions {
@@ -33,8 +70,8 @@ function FixFilePermissions {
         try {
             Set-Acl -Path $fileOrFolder.FullName -AclObject $Acl
         } catch [System.Management.Automation.WildcardPatternException] {
-            #ignore error of invalid filenames: [.exe and [.1.gz
-            Write-Host "Skipped invalid file $fileOrFolder.FullName"
+            #ignore error on invalid filenames: [.exe and [.1.gz
+            Write-Host "Skipped invalid file $fileOrFolder"
         }
     }
 }
@@ -46,7 +83,6 @@ function BabunIsInstalled {
 if (BabunIsInstalled) {
     Write-Host "Babun is already installed! Skipping..."
 } else {
-    Write-Host "Executing installation script..."
     InstallBabun
 
     Write-Host "Fixing file permissions..."
